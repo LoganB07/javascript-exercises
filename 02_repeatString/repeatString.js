bf4aabaa8ddd0fa86d2c39f792078d85c047408a
LoganB07 <nagol0725@gmail.com>
2024-10-08 12:39:00 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, numRepeated) {
+    if (numRepeated < 0){
+        return "ERROR";
+    }
+    let newString ='';
+    for (let i = 0; i < numRepeated; i++){
+        newString += string;
+    }
+    return newString;
 
 };
 
