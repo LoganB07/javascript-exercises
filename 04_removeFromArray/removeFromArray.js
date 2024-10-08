5c8186cf161b2d595f5c2b44dfe3e19d2f7a5200
LoganB07 <nagol0725@gmail.com>
2024-10-08 13:50:04 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numsToRemove) {
+    const nums = numsToRemove;
+   for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            console.log(`${array[i]} ${nums[j]}`);
+            if (array[i] === nums[j]) {
+                array.splice(i, 1);
+                console.log(`removed ${array}`);
+                i--;
+            }
+        }
+    }
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
