3fe4c54b54780ceed2adae3561f7b4f47791842c
LoganB07 <nagol0725@gmail.com>
2024-10-12 16:00:20 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    previous = 0;
+    current = 1;
+    num = parseInt(num);
+    if (num == 0) {return 0;}
+    if (num < 0) {return "OOPS";}
 
+    for (let i = 1; i < num; i++) {
+        let temp = current;
+        console.log(current);
+        current += previous;
+        previous = temp;
+    }
+
+    return current;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
