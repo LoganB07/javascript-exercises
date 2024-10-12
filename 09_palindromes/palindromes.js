887b3d81a21d70f16ea3582bf156d025244cc0b5
LoganB07 <nagol0725@gmail.com>
2024-10-12 15:44:26 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function palindromes(string) {
+    toString(string);
+    string = string.toLowerCase();
+    string = string.replace(/[^\w\']|_/g, "");
+    console.log(string);
 
+    let j = string.length -1;
+    for (let i = 0; i <= j; i++) {
+        console.log(string[i]); console.log(string[j]);
+        if (string[i] != string[j]) {return false;}
+        j--;
+    }
+    return true;
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
